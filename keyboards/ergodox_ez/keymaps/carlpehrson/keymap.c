5f81609e5541bd5a4b3951156fbb9bbac9fdda02
Carl Pehrson <carl.pehrson@tv4.se>
2018-05-08 12:23:30 +0200

    Add delete to symbol layer

--- keyboards/ergodox_ez/keymaps/carlpehrson/keymap.c
+++ keyboards/ergodox_ez/keymaps/carlpehrson/keymap.c
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                                        |      |      |       |      |      |
      *                                 ,------|------|------|       |------+------+------.
      *                                 |      |      |      |       |      |      |      |
-     *                                 |      |      |------|       |------|      |      |
+     *                                 |      |Delete|------|       |------|      |      |
      *                                 |      |      |      |       |      |      |      |
      *                                 `--------------------'       `--------------------'
      */
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_TRNS,KC_HASH,KC_PERC,TPREV,      TNEXT,
                                            KC_TRNS,KC_TRNS,
                                                    KC_TRNS,
-                                   KC_TRNS,KC_TRNS,KC_TRNS,
+                                   KC_TRNS,KC_DEL,KC_TRNS,
            // right hand
            KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,     KC_F11,
            KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    NO_ASTR,    KC_F12,
